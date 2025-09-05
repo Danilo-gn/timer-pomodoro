08d47894e8b6fd4cc553e1aeb32a0a74ca129893
Danilo-gn <dgndanilo@gmail.com>
2025-09-05 16:05:25 -0300

    add de arquivos icon e mp3, contador regressivo funcionando!

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,7 +1,9 @@
-import { useReducer } from "react";
+import { useEffect, useReducer } from "react";
 import { initialTaskState } from "./initialTaskState";
 import { TaskContext } from "./taskContext";
 import { taskReducer } from "./TaskReducer";
+import { TaskActionTypes } from "./TaskActions";
+import { TimerWorkerManager } from "../../workers/TimerWorkerManager";
 
 type TaskContextProviderProps = {
     children: React.ReactNode
@@ -9,6 +11,34 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({children}: TaskContextProviderProps) {
     const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+
+    const worker = TimerWorkerManager.getInstance();
+
+    worker.onmessage(e => {
+        const countDownSeconds = e.data
+        console.log(e.data);
+
+        if(countDownSeconds <= 0) {
+            dispatch({
+                type: TaskActionTypes.COMPLETE_TASK,
+            });
+            worker.terminate();
+        } else {
+            dispatch({
+                type: TaskActionTypes.COUNT_DOWN, 
+                payload: {secondsRemaining: countDownSeconds}
+            });
+        }
+    })
+
+    useEffect(() => {
+        if(!state.activeTask) {
+            console.log('worker terminado')
+            worker.terminate()
+        }
+
+        worker.postMessage(state)
+    }, [worker, state])
 
     return (
         <TaskContext.Provider value={{state, dispatch}}>
